ebaa3b102f0ab12426537e2f15047abd0654f21f
thuongton999 <thuongton0809999@gmail.com>
2022-07-02 16:41:37 +0700

    regenerate color palette

--- blogsphere/src/components/Modal.js
+++ blogsphere/src/components/Modal.js
@@ -9,9 +9,9 @@ export default function Modal({ children, active, onActivate, onDeactivate }) {
         if (typeof onDeactivate !== "function") return;
         onDeactivate();
     }, [active, onActivate, onDeactivate]);
-    return (
+    return active && (
         <div className="fixed inset-0 flex justify-center items-center backdrop-blur p-8">
-            <div className="secondary-color-bg h-fit p-8 rounded-lg">
+            <div className="nord2-bg h-fit p-8 rounded-lg">
                 {children}
             </div>
         </div>
